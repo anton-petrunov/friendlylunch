c3dba5f42b5d876504511343566fbad60f9c5c80
Anton Petrunov <a.n.tone@mail.ru>
2022-05-07 22:02:00 +0300

    Add CrudUserRepository

---
+++ src/main/java/ru/mygraduation/friendlylunch/repository/CrudUserRepository.java
@@ -0,0 +1,24 @@
+package ru.mygraduation.friendlylunch.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.mygraduation.friendlylunch.model.User;
+
+import javax.persistence.QueryHint;
+
+@Transactional(readOnly = true)
+public interface CrudUserRepository extends JpaRepository<User, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    @QueryHints({
+            @QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false")
+    })
+    User getByEmail(String email);
+}
