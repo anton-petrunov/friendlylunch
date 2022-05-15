b808e67593e0344ef2b15464e8767a77c694febb
Anton Petrunov <a.n.tone@mail.ru>
2022-05-15 09:57:35 +0300

    Fix populateDB admin/user credentials

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,8 +11,8 @@ VALUES ('Shaurma', 'shaurma=180;coffee=60.00', now()),
        ('Funny Fish', 'Fish stake=310.9;Sushi=500.20', now());
 
 INSERT INTO users (NAME, EMAIL, PASSWORD, VOTED_FOR, VOTING_DATE_TIME)
-VALUES ('Anton', 'a.n.tone@mail.ru', 'psswd', 100003, '2022-04-28 22:36:00'),
-       ('Kirill', 'gilly@amail.ru', 'password', 100002, now()),
+VALUES ('User', 'user@yandex.ru', 'password', 100003, '2022-04-28 22:36:00'),
+       ('Admin', 'admin@gmail.com', 'admin', 100002, now()),
        ('Annet', 'gmail@gmail.com', 'pass', 100001, '2022-04-29 11:00:00'),
        ('Pirouette', 'mao@mao.ru', 'smoke', 100000, '2022-05-01 15:00:00'),
        ('Man whithout vote', 'man@without.vote', '', null, null);
