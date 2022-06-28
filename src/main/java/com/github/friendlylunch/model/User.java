90124b1db358e19087544bffe489864c53ce93b6
Anton Petrunov <a.n.tone@mail.ru>
2022-06-28 12:02:45 +0300

    Update User model and repository

--- src/main/java/com/github/friendlylunch/model/User.java
+++ src/main/java/com/github/friendlylunch/model/User.java
@@ -1,10 +1,9 @@
 package com.github.friendlylunch.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.lang.Nullable;
 import org.springframework.util.CollectionUtils;
 
 import javax.persistence.*;
@@ -14,7 +13,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Date;
 import java.util.EnumSet;
@@ -37,18 +35,11 @@ public class User extends AbstractNamedEntity implements Serializable {
     @Size(min = 5, max = 128)
     private String password;
 
-    @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()")
+    @Column(name = "registered", nullable = false,
+            columnDefinition = "timestamp default now()", updatable = false)
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date registered = new Date();
-
-    @Column(name = "voted_for", columnDefinition = "integer default null")
-    @Nullable
-    private Integer votedFor;
-
-    @Column(name = "voting_date_time", columnDefinition = "timestamp default null")
-    @Nullable
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
-    private LocalDateTime votingDateTime;
 
     @Enumerated(EnumType.STRING)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
@@ -61,78 +52,47 @@ public class User extends AbstractNamedEntity implements Serializable {
     private Set<Role> roles;
 
     public User(Integer id, String name, String email,
-                String password, Date registered, Integer votedFor,
-                LocalDateTime votingDateTime, Collection<Role> roles) {
+                String password, Date registered, Collection<Role> roles) {
         super(id, name);
         this.email = email;
         this.password = password;
         this.registered = registered;
-        this.votedFor = votedFor;
-        this.votingDateTime = votingDateTime;
         setRoles(roles);
     }
 
     public User() {
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Integer getVotedFor() {
-        return votedFor;
-    }
-
-    public LocalDateTime getVotingDateTime() {
-        return votingDateTime;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setVotedFor(int votedFor) {
-        this.votedFor = votedFor;
-    }
-
-    public void setVotingDateTime(LocalDateTime votingDateTime) {
-        this.votingDateTime = votingDateTime;
     }
 
     public Set<Role> getRoles() {
         return roles;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public void setRoles(Collection<Role> roles) {
         this.roles = CollectionUtils.isEmpty(roles) ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
     }
 
     @Override
@@ -143,8 +103,6 @@ public class User extends AbstractNamedEntity implements Serializable {
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", registered=" + registered +
-                ", votedFor=" + votedFor +
-                ", votingDateTime=" + votingDateTime +
                 ", roles=" + roles +
                 '}';
     }
