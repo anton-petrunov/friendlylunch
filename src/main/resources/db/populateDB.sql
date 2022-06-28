53c3366ee987aed3c52e1474175bdfe1c4ff7181
Anton Petrunov <a.n.tone@mail.ru>
2022-06-28 12:05:43 +0300

    Update DB according new model

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -4,20 +4,34 @@ DELETE
 FROM RESTAURANTS;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO RESTAURANTS (NAME, DISHES, UPDATING_DATE_TIME)
-VALUES ('Shaurma', '{"Shaurma": 180, "Coffee": 69}', now()),
-       ('Fresh Apple Lunches', '{"apple": 23, "orange": 49, "mango": 169}', '2022-04-28 18:30:00'),
-       ('Papa Grill', '{"Chicken grill": 299, "Cow grill": 349}', '2022-05-01 14:58:00'),
-       ('Funny Fish', '{"Fish stake": 310, "Sushi": 500}', now());
-
-INSERT INTO users (NAME, EMAIL, PASSWORD, VOTED_FOR, VOTING_DATE_TIME)
-VALUES ('User', 'user@yandex.ru', '{noop}password', 100003, '2022-04-28 22:36:00'),
-       ('Admin', 'admin@gmail.com', '{noop}admin', 100002, now()),
-       ('Annet', 'gmail@gmail.com', '{noop}pass', 100001, '2022-04-29 11:00:00'),
-       ('Pirouette', 'mao@mao.ru', '{noop}smoke', 100000, '2022-05-01 15:00:00'),
-       ('Man whithout vote', 'man@without.vote', '{noop}', null, null);
+INSERT INTO users (NAME, EMAIL, PASSWORD)
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin'),
+       ('Annet', 'gmail@gmail.com', '{noop}pass'),
+       ('Pirouette', 'mao@mao.ru', '{noop}smoke'),
+       ('Man whithout vote', 'man@without.vote', '{noop}');
 
 INSERT INTO user_roles (role, user_id)
-VALUES ('USER', 100004),
-       ('ADMIN', 100005),
-       ('USER', 100005);
+VALUES ('USER', 100000),
+       ('ADMIN', 100001),
+       ('USER', 100001);
+
+INSERT INTO RESTAURANTS (NAME)
+VALUES ('Shaurma Number ONE'),
+       ('Fresh Apple Lunches'),
+       ('Papa Grill'),
+       ('Funny Fish');
+
+INSERT INTO MENUS (RESTAURANT_ID, DATE)
+VALUES (100005, '2022-6-24'),
+       (100006, '2022-6-25'),
+       (100007, now),
+       (100008, now);
+
+INSERT INTO DISHES (MENU_ID, NAME, PRICE)
+VALUES (100009, 'Coca-Cola', 140),
+       (100009, 'Pizza Formaggio', 490),
+       (100010, 'Chief Burger', 219);
+
+INSERT INTO VOTES (MENU_ID, USER_ID)
+VALUES (100009, 100002);
