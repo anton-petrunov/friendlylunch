203965951bf39e88df4c94dc9c43f5c533585723
Anton Petrunov <a.n.tone@mail.ru>
2022-07-01 17:19:42 +0300

    Fix JSON lazy writing. Add JacksonObjectMapper

--- src/main/java/com/github/friendlylunch/web/json/JsonUtil.java
+++ src/main/java/com/github/friendlylunch/web/json/JsonUtil.java
@@ -1,4 +1,4 @@
-package com.github.friendlylunch.util;
+package com.github.friendlylunch.web.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
