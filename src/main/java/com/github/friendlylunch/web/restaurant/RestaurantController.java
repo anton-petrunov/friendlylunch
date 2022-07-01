9439bce01ac5c1a082c0fc766b5af8a6e71d5562
Anton Petrunov <a.n.tone@mail.ru>
2022-07-01 17:08:57 +0300

    Add Restaurant controllers

---
+++ src/main/java/com/github/friendlylunch/web/restaurant/RestaurantController.java
@@ -0,0 +1,46 @@
+package com.github.friendlylunch.web.restaurant;
+
+import com.github.friendlylunch.model.Restaurant;
+import com.github.friendlylunch.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.github.friendlylunch.util.ValidationUtil.assureIdConsistent;
+import static com.github.friendlylunch.util.ValidationUtil.checkNew;
+
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public Restaurant create(Restaurant restaurant) {
+        checkNew(restaurant);
+        return restaurantRepository.save(restaurant);
+    }
+
+    public void update(Restaurant restaurant, int id) {
+        assureIdConsistent(restaurant, id);
+        restaurantRepository.save(restaurant);
+    }
+
+    public void delete(int id) {
+        restaurantRepository.delete(id);
+    }
+
+    public Restaurant get(int id) {
+        return restaurantRepository.findById(id).orElse(null);
+    }
+
+    public List<Restaurant> getAll() {
+        return restaurantRepository.findAll();
+    }
+
+    public Restaurant getWithMenus(int id) {
+        return restaurantRepository.getWithMenus(id);
+    }
+
+    public Restaurant getWithMenusAndDishes(int id) {
+        return restaurantRepository.getWithMenusAndDishes(id);
+    }
+}
---
+++ src/main/java/com/github/friendlylunch/web/restaurant/RestaurantRestController.java
@@ -0,0 +1,66 @@
+package com.github.friendlylunch.web.restaurant;
+
+import com.github.friendlylunch.model.Restaurant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = RestaurantRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestaurantRestController extends RestaurantController {
+
+    static final String REST_URL = "/rest/admin/restaurants";
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        Restaurant created = super.create(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @Override
+    @PutMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Restaurant restaurant, @PathVariable int id) {
+        super.update(restaurant, id);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<Restaurant> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @GetMapping("/{id}/with-menus")
+    public Restaurant getWithMenus(@PathVariable int id) {
+        return super.getWithMenus(id);
+    }
+
+    @Override
+    @GetMapping("/{id}/with-meals-dishes")
+    public Restaurant getWithMenusAndDishes(@PathVariable int id) {
+        return super.getWithMenusAndDishes(id);
+    }
+}
